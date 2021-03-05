249ada4bc7395a47d9e3083aee1a461b3d5a09e4
Larozox <samisua16@gmail.com>
2021-03-05 10:47:40 -0500

    modal

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Modal, Text, View, Button } from 'react-native';
 
 //Guardar en una constante, el ancho del dispositivo
 //const width = Dimensions.get('window').width
 
 export default function App() {
+  const [modal,setModal] = useState(false)
   return (
     <View style={styles.container}>
-      <ActivityIndicator 
-      size = "large"
-      color = "#0000ff"
+      <Modal
+        animationType = "slide"
+        transparent = {true}
+        visible = {modal}
+      >
+        <View style={styles.center}>
+          <View style={styles.content}>
+            <Text>Modal</Text>
+            <Button
+            title="Cerrar modal"
+            onPress ={()=>setModal(!modal)}
+            />
+          </View>
+        </View>
+      </Modal>
+      <Button title="Abrir modal"
+      onPress = {()=> setModal(!modal)}
       />
     </View>
   );
@@ -22,5 +37,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  content: {
+    backgroundColor: '#fff'
+  },
+  center : {
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
